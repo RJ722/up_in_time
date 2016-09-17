97bf59f2c95c50d1ab7421c654101ec138e58c68
Rahul <rahul722j@gmail.com>
2016-09-17 18:17:29 +0530

    Add moving effect in nav_bar, change bg

--- static/alarm/JS/base.js
+++ static/alarm/JS/base.js
@@ -1,7 +1,21 @@
+/*
 $(function(){
 	$('.nav_button a').click(function(){
         $('.nav_button .active').removeClass('active'); // remove the class from the currently selected
         $(this).addClass('active'); // add the class to the newly clicked link
     	$(this).addClass('nav_button');
     });
+});
+*/
+
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 25) {
+    $('.header_main').addClass('shrink');
+    $('.nav_button ').addClass('shrink');
+    $('#nav_bar_right tr').addClass('shrink')
+  } else {
+    $('.header_main').removeClass('shrink');
+    $('.nav_button ').removeClass('shrink');
+    $('#nav_bar_right tr').removeClass('shrink')
+  }
 });
